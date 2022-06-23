e624021fbd697ab6bd6717269571de71f54fcbd6
nfernandezsanz <nicolas@fernandezsanz.com>
2022-06-23 17:48:03 -0300

    View Admin

--- api/cra/crypto/models.py
+++ api/cra/crypto/models.py
@@ -14,13 +14,20 @@ class Crypto(models.Model):
         return price
 
     def __str__(self):
-        return self.name + f"({self.symbol})"
+        return self.name + f" ({self.symbol.upper()})"
 
-
+    class Meta:
+        ordering = ['name']
+        
 class PriceRecord(models.Model):
     crypto     = models.ForeignKey(Crypto, on_delete=models.CASCADE, default=None)
     timestamp  = models.DateTimeField(auto_now_add=True)
     price      = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f"{self.crypto.symbol.upper()} ({self.timestamp.date()}) --> {round(self.price,2)}USD"
+    class Meta:
+        ordering = ['timestamp']
 
 
 class Sentiment(models.Model):
@@ -43,11 +50,22 @@ class Analisis(models.Model):
 class Source(models.Model):
     name = models.CharField(max_length=128, blank=False, unique=True)
     
+    def __str__(self):
+        return self.name
+
     @property
     def afinity(self):
         opinions = Opinion.objects.filter(source=self).aggregate(af = Avg('analisis__sentiment'))
         return opinions['af']
     
+    @property
+    def afinity_str(self):
+        if(self.afinity > 0):
+            return "CRYPTO-AMIGO"
+        elif(self.afinity < 0):
+            return "ANTI-CRYPTO"
+        else:
+            return "NEUTRAL"
 class Opinion(models.Model):
     crypto     = models.ForeignKey(Crypto, on_delete=models.CASCADE, default=None)
     timestamp  = models.DateTimeField(auto_now_add=True)
@@ -55,3 +73,5 @@ class Opinion(models.Model):
     link       = models.TextField()
     analisis   = models.ForeignKey(Analisis, on_delete=models.CASCADE)
     sentiment  = models.ForeignKey(Sentiment, on_delete=models.CASCADE, default=None)
+    class Meta:
+        ordering = ['timestamp']
